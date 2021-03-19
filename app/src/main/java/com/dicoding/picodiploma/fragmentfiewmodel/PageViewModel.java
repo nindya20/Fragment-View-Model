729ec842d4fdeb30e7c3bb763767089cf6583ecb
nindya20 <nindya_aprilia_28rpl@student.smktelkom-mlg.sch.id>
2021-03-19 21:28:10 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/picodiploma/fragmentfiewmodel/PageViewModel.java
@@ -0,0 +1,18 @@
+package com.dicoding.picodiploma.fragmentfiewmodel;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class PageViewModel extends ViewModel {
+
+    private MutableLiveData<String> mName = new MutableLiveData<>();
+
+    public LiveData<String> getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName.setValue(name);
+    }
+}
